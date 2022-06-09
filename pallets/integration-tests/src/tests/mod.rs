a251ed5fc550662adc88d2a907a0bd2ccc2cfaa6
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2022-06-09 16:31:35 +0200

    move tests into separate files

---
+++ pallets/integration-tests/src/tests/mod.rs
@@ -0,0 +1,17 @@
+pub mod reactions;
+
+/// Tests for posts_pallet
+pub mod posts;
+pub mod comments;
+pub mod shared_post;
+
+pub mod spaces;
+
+mod profiles;
+
+mod space_follows;
+
+mod profile_follows;
+
+mod space_ownership;
+
